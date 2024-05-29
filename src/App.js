e7f8f66b71e7594ab9761110ae4418d83ac43acd
milamocha <d00253640@student.dkit.ie>
2024-05-29 01:34:10 +0100

    Removed starter react code. Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
 
+// TODO replace logo192.png in manifest.json
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Works! - Mila :D
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <p>Main text area</p>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div>
+                <h3>Mila Murphy</h3>
+            </div>
+            <div>
+            <a href="#">Home</a>
+            <a href="#">Tools</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
