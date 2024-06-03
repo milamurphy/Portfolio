f1b63873479fb7ea60b4d464a3eb97f51ee80583
milamocha <d00253640@student.dkit.ie>
2024-06-03 03:40:43 +0100

    Added icons and styles to home container section. Changed title

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,18 @@ function Home() {
     return (
         <div className="homeContainer">
             <div className="home">
-                <p>Hi there, I'm</p>
+                <div className="greetingText">Hi there, I'm</div>
+                <div className="milaText">Mila Murphy</div>
+                <div className="softwareText">Software Developer</div>
+                <div className="homeTextContainer">
+                    I’m passionate about transforming ideas into reality through software. Currently seeking an internship as part of my academic course.
+                </div>
+                <div className="buttonsContainer">
+                    <button><img src="https://img.icons8.com/fluency-systems-filled/48/99ffb5/regular-document.png" alt="resume icon" />Resume</button>
+                    <button><img src="https://img.icons8.com/fluency-systems-filled/48/99ffb5/github.png" alt="github icon" />GitHub</button>
+                    <button><img src="https://img.icons8.com/fluency-systems-filled/48/99ffb5/linkedin.png" alt="linkedin icon" />LinkedIn</button>
+                    <button><img src="https://img.icons8.com/fluency-systems-filled/48/99ffb5/new-post.png" alt="email icon" />Email</button>
+                </div>
             </div>
         </div>
     );
--- src/Navbar.js
+++ src/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
                 <a href="#">About</a>
                 <a href="#">Projects</a>
                 <a href="#">Contact</a>
+                <img width="25" height="25" src="https://img.icons8.com/fluency-systems-filled/48/FFFFFF/crescent-moon.png" alt="moon icon"/>
             </div>
         </div>
     );
