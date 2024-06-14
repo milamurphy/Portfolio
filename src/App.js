8c9e06f73f4b753690617e285547c5ccbdb378b0
milamocha <d00253640@student.dkit.ie>
2024-06-14 15:27:13 +0100

    Added gradient that follows cursor to App.js Changed transparency of buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import Projects from './components/Projects';
+import './index.css';
 
 // TODO replace logo192.png in manifest.json
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [gradientSize, setGradientSize] = useState(500);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="App">
+      <div id="gradientContainer">
+        <div
+          id="gradientBackground"
+          style={{
+            width: `${gradientSize}px`,
+            height: `${gradientSize}px`,
+            transform: `translate(${mousePosition.x - gradientSize / 2}px, ${
+              mousePosition.y - gradientSize / 2
+            }px)`,
+          }}
+        />
+      </div>
       <Navbar/>
       <Home/>
       <About/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <div className="projectsContainer">
+            <div class="projectColumn">
+                Project 1
+            </div>
+            <div class="projectColumn">
+                Project 2
+            </div>
+            <div class="projectColumn">
+                Project 3
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
