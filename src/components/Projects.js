fde091f0d25883b48ab5cdf4413a3b9882cde859
milamocha <d00253640@student.dkit.ie>
2024-06-19 03:34:51 +0100

    3 column projects section created with basic styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,31 @@
 import React from "react";
+import temp1 from '../images/temp1.png';
+import temp2 from '../images/temp2.png';
+import temp3 from '../images/temp3.png';
 
 function Projects() {
     return (
         <div className="projectsContainer">
-            <div class="projectColumn">
-                Project 1
+            <div className="projectColumn">
+                <div className="project">
+                    <img src={temp1} alt="Project 1" />
+                    <div className="projectTitle">Picnet</div>
+                    <div className="projectDesc">All the resources you will need to get a boost into economics.</div>
+                </div>
             </div>
             <div class="projectColumn">
-                Project 2
+                <div className="project">
+                    <img src={temp2} alt = "Project 2"/>
+                    <div className="projectTitle">Bug's Life</div>
+                    <div className="projectDesc">All the resources you will need to get a boost into economics.</div>
+                </div>
             </div>
             <div class="projectColumn">
-                Project 3
+                <div className="project">
+                    <img src={temp3} alt = "Project 3"/>
+                    <div className="projectTitle">StyleKoi</div>
+                    <div className="projectDesc">All the resources you will need to get a boost into economics.</div>
+                </div>
             </div>
         </div>
     );
